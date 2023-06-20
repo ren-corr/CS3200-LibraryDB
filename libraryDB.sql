13a11f85f1cab1cd50dc3fc7dc3fa5b9739e3fb2
Morgan Spencer <morganspencer@Morgans-MacBook-Pro.local>
2023-06-19 22:25:28 -0400

    Update createUser proc

--- libraryDB.sql
+++ libraryDB.sql
@@ -370,26 +370,33 @@ END //
 DELIMITER ;
 
 -- register new patron
-DROP PROCEDURE IF EXISTS createUser;
+DROP PROCEDURE IF EXISTS newPatron;
 DELIMITER //
-CREATE PROCEDURE createUser(
-IN library_card_number INT,
+CREATE PROCEDURE newPatron(
 IN p_pin_number INT,
 IN p_first_name VARCHAR(50),
 IN p_last_name VARCHAR(50),
 IN p_address VARCHAR(50)
 )
 BEGIN
-	-- check if patron already exists
-    IF EXISTS (SELECT 1 FROM patron WHERE library_card_number = p_library_card_number) THEN
-		SIGNAL SQLSTATE '45000'
-			SET MESSAGE_TEXT = 'Patron already exists.';
+	DECLARE p_library_card_number INT;
+    
+    -- check if patron already exists
+    IF EXISTS (
+		SELECT 1 FROM patron
+        WHERE pin_number = p_pin_number
+        AND first_name = p_first_name
+        AND last_name = p_last_name
+        AND address = p_address) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Patron already exists.';
 	ELSE
+		SET p_library_card_number = LAST_INSERT_ID();
+
 		-- insert new patron
 		INSERT INTO patron(library_card_number, pin_number, first_name, last_name, address)
 		VALUES (p_library_card_number, p_pin_number, p_first_name, p_last_name, p_address);
-		
-        SELECT 'Patron added successfully!' AS MESSAGE;
+        
+		SELECT 'Patron added successfully!' AS MESSAGE;
 	END IF;
 END //
 DELIMITER ;
